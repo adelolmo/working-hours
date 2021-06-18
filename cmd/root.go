8806721e5ce0f96431fc192a9965e9b5eaa80726
adelolmo <andoni.delolmo@signavio.com>
2021-06-18 18:45:38 +0200

    prepare release v1.0.0

--- cmd/root.go
+++ cmd/root.go
@@ -13,64 +13,60 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+
 package cmd
 
 import (
-  "fmt"
-  "os"
-  "github.com/spf13/cobra"
-
-  homedir "github.com/mitchellh/go-homedir"
-  "github.com/spf13/viper"
-
+	"fmt"
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"os"
 )
-
 
 var cfgFile string
 
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-  Use:   "wh",
-  Short: "Working-Hours is a tool for tracking how long you work",
-  Long: `Working-Hours is a tool for tracking how long you work.
+	Use:   "wh",
+	Short: "Working-Hours is a tool for tracking how long you work",
+	Long: `Working-Hours is a tool for tracking how long you work.
 It provides reports with insights about your working days.
 `,
+	Version: "1.0.0",
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-  if err := rootCmd.Execute(); err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
 
 func init() {
-  cobra.OnInitialize(initConfig)
+	cobra.OnInitialize(initConfig)
 }
-
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-  if cfgFile != "" {
-    viper.SetConfigFile(cfgFile)
-  } else {
-    home, err := homedir.Dir()
-    if err != nil {
-      fmt.Println(err)
-      os.Exit(1)
-    }
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 
-    viper.AddConfigPath(home)
-    viper.SetConfigName(".working-hours")
-  }
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".working-hours")
+	}
 
-  viper.AutomaticEnv()
+	viper.AutomaticEnv()
 
-  if err := viper.ReadInConfig(); err == nil {
-    fmt.Println("Using config file:", viper.ConfigFileUsed())
-  }
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
 }
-
